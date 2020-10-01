d3b4e1c9906f9dbb58d21cab03692d2bbcec1852
Hyun2 <hks0284@gmail.com>
2020-10-01 23:26:29 +0900

    allowed hosts 변경

--- rental/settings.py
+++ rental/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["ml-rental.herokuapp.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["ml-rental-be.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
