020119248741f2181cd52a90192fccd5bd7f8e76
Hyun2 <hks0284@gmail.com>
2020-10-01 20:32:42 +0900

    static root 추가 in settings.py

--- rental/settings.py
+++ rental/settings.py
@@ -134,6 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 SHELL_PLUS_PRE_IMPORTS = [
     ("rental.utils", "bulk_insert"),
